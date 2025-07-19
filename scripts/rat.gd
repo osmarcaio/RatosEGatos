ff06ecc68390896f7bbe5bf3af953325d1d2b371
PathelerPyth <pathleralfpyth@gmail.com>
2025-07-19 14:11:25 -0300

    Criação da malha de navegação
    Foi criado o tilemap e area de pathfinding.
    Foi alterado um pouco o script do rato.
    Algumas cenas foram adicionadas.

--- scripts/rat.gd
+++ scripts/rat.gd
@@ -2,24 +2,37 @@ extends CharacterBody2D
 
 
 var dragging = false
-var line_0 : Vector2 = position
-var line_1 : Vector2 = position
-var line_final : Vector2 = position
 
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
-func _process(delta: float) -> void:
+var speed = 100.0
+
+func _ready():
+	# Começa com pathfinding ativo
+	navigation_agent_2d.target_position = global_position
+
+func _physics_process(delta):
 	queue_redraw()
+	
+	if navigation_agent_2d.is_navigation_finished():
+		return
+	
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	var direction = (next_path_position - global_position).normalized()
+	velocity = direction * speed
+	move_and_slide()
+
+func set_target_position(pos: Vector2):
+	navigation_agent_2d.target_position = pos
 
 func _draw() -> void:
 	if dragging:
-		line_1 = get_global_mouse_position()
-		line_final = line_1 - line_0
+		var line_final = get_global_mouse_position() - position
 		draw_line(Vector2(0, 0), line_final, Color.WHITE, 2)
 
 func _on_button_button_down() -> void:
-	line_0 = position
 	dragging = true
 
 func _on_button_button_up() -> void:
 	dragging = false
-	position = get_global_mouse_position()
+	set_target_position(get_global_mouse_position())
