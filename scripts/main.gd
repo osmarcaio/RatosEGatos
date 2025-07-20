35cb4d3ec4f0374435abe4000dde2e194d8d9507
PathelerPyth <pathleralfpyth@gmail.com>
2025-07-19 22:17:37 -0300

    Mecanica do Rato quase completa

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,32 @@
 extends Node2D
 
 
+var rat_scene := preload("res://scenes/rat.tscn")
+
+@onready var modes_position := {
+	"waiter" = $Markers/WaiterPosition.position,
+	"chef" = $Markers/ChefPosition.position
+}
+@onready var rats: Node2D = $Rats
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	pass
+
+func _unhandled_input(event): # Ao clicar em nenhum botão
+	if event is InputEventMouseButton and event.pressed:
+		var focused = get_viewport().gui_get_focus_owner()
+		if focused:
+			focused.release_focus()
+
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			spawn_rat()
+
+func spawn_rat():
+	var rat_instance = rat_scene.instantiate()
+	rat_instance.position = $Markers/StarterPosition.position
+	rat_instance.set_modes_position(modes_position)
+	rats.add_child(rat_instance)
+	rat_instance.set_target_position($Markers/EntryPosition.position, 16) 
--- scripts/rat.gd
+++ scripts/rat.gd
@@ -1,19 +1,31 @@
 extends CharacterBody2D
 
+var dragging := false
+var speed := 50.0
+var mode := "waiter" # "waiter" or "chef"
+var modes_position := {
+	"waiter": Vector2(100, 100),
+	"chef": Vector2(100, 100)
+}
 
-var dragging = false
+@onready var rat_sprites := {
+	"waiter": preload("res://assets/sprites/rat_waiter2.png"),
+	"chef": preload("res://assets/sprites/rat_chef2.png")
+}
 
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var select: Control = $Select
+@onready var select_pop_timer: Timer = $SelectPopTimer
+@onready var rat_sprite: Sprite2D = $RatSprite
 
-var speed = 100.0
 
 func _ready():
-	# Começa com pathfinding ativo
 	navigation_agent_2d.target_position = global_position
 
-func _physics_process(delta):
+func _process(delta: float) -> void:
 	queue_redraw()
-	
+
+func _physics_process(delta):	
 	if navigation_agent_2d.is_navigation_finished():
 		return
 	
@@ -22,8 +34,14 @@ func _physics_process(delta):
 	velocity = direction * speed
 	move_and_slide()
 
-func set_target_position(pos: Vector2):
-	navigation_agent_2d.target_position = pos
+func set_target_position(base:Vector2, variation=0):
+	var random_vec := Vector2(
+		randf_range(-variation, variation),
+		randf_range(-variation, variation)
+	)
+	print(base, random_vec, base + random_vec)
+	
+	navigation_agent_2d.target_position = base + random_vec
 
 func _draw() -> void:
 	if dragging:
@@ -36,3 +54,27 @@ func _on_button_button_down() -> void:
 func _on_button_button_up() -> void:
 	dragging = false
 	set_target_position(get_global_mouse_position())
+
+func _on_rat_button_focus_entered() -> void:
+	select.visible = true
+
+func _on_rat_button_focus_exited() -> void:
+	select_pop_timer.start()
+
+func _on_select_pop_timer_timeout() -> void:
+	select.visible = false
+
+func _on_chef_button_pressed() -> void:
+	set_mode("chef")
+
+func _on_waiter_button_pressed() -> void:
+	set_mode("waiter")
+
+func set_modes_position(new_modes_position) -> void:
+	modes_position = new_modes_position
+	print(modes_position)
+	
+func set_mode(mode):
+	mode = mode
+	rat_sprite.texture = rat_sprites[mode]
+	set_target_position(modes_position[mode], 24)
